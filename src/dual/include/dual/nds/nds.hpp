6b8df759e4342ec5447be72bb5d4185f1c5e27fa
fleroviux <fleroviux@gmail.com>
2023-04-04 19:00:27 +0200

    Add a basic NDS class

---
+++ src/dual/include/dual/nds/nds.hpp
@@ -0,0 +1,12 @@
+
+#pragma once
+
+namespace dual::nds {
+
+class NDS {
+  public:
+
+  private:
+};
+
+} // namespace dual::nds
---
+++ src/dual/src/nds/nds.cpp
@@ -0,0 +1,6 @@
+
+#include <dual/nds/nds.hpp>
+
+namespace dual::nds {
+
+} // namespace dual::nds
