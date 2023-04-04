0412beae9ff70ce710e31df92ca3d1f8cae562cd
fleroviux <fleroviux@gmail.com>
2023-04-04 19:07:59 +0200

    Setup atom logging

--- src/platform/sdl/src/main.cpp
+++ src/platform/sdl/src/main.cpp
@@ -1,8 +1,13 @@
 
+#include <atom/logger/sink/console.hpp>
+#include <atom/logger/logger.hpp>
+
 #include "application.hpp"
 
 #undef main
 
 int main(int argc, char** argv) {
+  atom::get_logger().InstallSink(std::make_shared<atom::LoggerConsoleSink>());
+
   return Application{}.Run(argc, argv);
 }
