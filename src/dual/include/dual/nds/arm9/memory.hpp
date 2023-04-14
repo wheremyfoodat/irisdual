371d23abf585f3f81e1ab048795cf9056cfc63ed
fleroviux <fleroviux@gmail.com>
2023-04-15 01:22:04 +0200

    Move ARM9 memory access code into a translation unit

--- src/dual/include/dual/nds/arm9/memory.hpp
+++ src/dual/include/dual/nds/arm9/memory.hpp
@@ -17,64 +17,17 @@ namespace dual::nds::arm9 {
     public:
       explicit MemoryBus(SystemMemory& memory) : m_ewram{memory.ewram.data()} {}
 
-      u8 ReadByte(u32 address, Bus bus) override {
-        return Read<u8>(address, bus);
-      }
+      u8  ReadByte(u32 address, Bus bus) override;
+      u16 ReadHalf(u32 address, Bus bus) override;
+      u32 ReadWord(u32 address, Bus bus) override;
 
-      u16 ReadHalf(u32 address, Bus bus) override {
-        return Read<u16>(address, bus);
-      }
-
-      u32 ReadWord(u32 address, Bus bus) override {
-        return Read<u32>(address, bus);
-      }
-
-      void WriteByte(u32 address, u8 value, Bus bus) override {
-        Write<u8>(address, value, bus);
-      }
-
-      void WriteHalf(u32 address, u16 value, Bus bus) override {
-        Write<u16>(address, value, bus);
-      }
-
-      void WriteWord(u32 address, u32 value, Bus bus) override {
-        Write<u32>(address, value, bus);
-      }
+      void WriteByte(u32 address, u8  value, Bus bus) override;
+      void WriteHalf(u32 address, u16 value, Bus bus) override;
+      void WriteWord(u32 address, u32 value, Bus bus) override;
 
     private:
-      template<typename T> T Read(u32 address, Bus bus) {
-        address &= ~(sizeof(T) - 1u);
-
-        switch(address >> 24) {
-          case 0x02: {
-            return atom::read<T>(m_ewram, address & 0x3FFFFFu);
-          }
-          case 0x04: {
-            ATOM_ERROR("arm9: unhandled {}-bit IO read from 0x{:08X}", bit::number_of_bits<T>(), address);
-            return 0;
-          }
-        }
-
-        ATOM_PANIC("unhandled {}-bit read from 0x{:08X}", bit::number_of_bits<T>(), address);
-      }
-
-      template<typename T> void Write(u32 address, T value, Bus bus) {
-        address &= ~(sizeof(T) - 1u);
-
-        switch(address >> 24) {
-          case 0x02: {
-            atom::write<T>(m_ewram, address & 0x3FFFFFu, value);
-            break;
-          }
-          case 0x04: {
-            ATOM_ERROR("arm9: unhandled {}-bit IO write to 0x{:08X} = 0x{:08X}", bit::number_of_bits<T>(), address, value);
-            break;
-          }
-          default: {
-            ATOM_PANIC("unhandled {}-bit write to 0x{:08X} = 0x{:08X}", bit::number_of_bits<T>(), address, value);
-          }
-        }
-      }
+      template<typename T> T    Read (u32 address, Bus bus);
+      template<typename T> void Write(u32 address, T value, Bus bus);
 
       u8* m_ewram;
   };
---
+++ src/dual/src/nds/arm9/memory.cpp
@@ -0,0 +1,64 @@
+
+#include <dual/nds/arm9/memory.hpp>
+
+namespace dual::nds::arm9 {
+
+  template<typename T> T MemoryBus::Read(u32 address, Bus bus) {
+    address &= ~(sizeof(T) - 1u);
+
+    switch(address >> 24) {
+      case 0x02: {
+        return atom::read<T>(m_ewram, address & 0x3FFFFFu);
+      }
+      case 0x04: {
+        ATOM_ERROR("arm9: unhandled {}-bit IO read from 0x{:08X}", bit::number_of_bits<T>(), address);
+        return 0;
+      }
+    }
+
+    ATOM_PANIC("unhandled {}-bit read from 0x{:08X}", bit::number_of_bits<T>(), address);
+  }
+
+  template<typename T> void MemoryBus::Write(u32 address, T value, Bus bus) {
+    address &= ~(sizeof(T) - 1u);
+
+    switch(address >> 24) {
+      case 0x02: {
+        atom::write<T>(m_ewram, address & 0x3FFFFFu, value);
+        break;
+      }
+      case 0x04: {
+        ATOM_ERROR("arm9: unhandled {}-bit IO write to 0x{:08X} = 0x{:08X}", bit::number_of_bits<T>(), address, value);
+        break;
+      }
+      default: {
+        ATOM_PANIC("unhandled {}-bit write to 0x{:08X} = 0x{:08X}", bit::number_of_bits<T>(), address, value);
+      }
+    }
+  }
+
+  u8 MemoryBus::ReadByte(u32 address, Bus bus) {
+    return Read<u8>(address, bus);
+  }
+
+  u16 MemoryBus::ReadHalf(u32 address, Bus bus) {
+    return Read<u16>(address, bus);
+  }
+
+  u32 MemoryBus::ReadWord(u32 address, Bus bus) {
+    return Read<u32>(address, bus);
+  }
+
+  void MemoryBus::WriteByte(u32 address, u8 value, Bus bus) {
+    Write<u8>(address, value, bus);
+  }
+
+  void MemoryBus::WriteHalf(u32 address, u16 value, Bus bus) {
+    Write<u16>(address, value, bus);
+  }
+
+  void MemoryBus::WriteWord(u32 address, u32 value, Bus bus) {
+    Write<u32>(address, value, bus);
+  }
+
+} // namespace dual::nds::arm9
